673a89051beaa361d308c82ab4d54c0bcb51743d
Jonas Rafael Duarte dos Santos <0205917@feevale.br>
2021-05-24 19:23:09 -0300

    '.'

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// openssl genrsa -out key.pem 2048
+// openssl req -new -key key.pem -out csr.epm
+// openssl x509 -req -days 365 -in csr.epm -signkey key.pem -out cert.pem
+
+const express = require("express");
+const axios = require("axios");
+const http = require("http");
+
+// create new express app and save it as "app"
+const app = express();
+
+app.all("*", async (req, res) => {
+  console.log(req.url, req.originalUrl);
+
+  // const { data } = await axios({
+  //   ...req,
+  // });
+
+  res.send("adsasdas");
+});
+
+app.listen(80);
